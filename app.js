8334c9e059e7461e419ced4605c8af96abae924c
Vika_Bob <vikabobir32@gmail.com>
2022-06-30 15:05:04 +0300

    test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-  res.send('Hello, friend');
+  res.send('Hello, friend. Version 2.0');
 });
 
 const port = process.env.PORT || '3000';
